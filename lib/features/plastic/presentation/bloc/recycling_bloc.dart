a28635ab88e997048b345d7f8b96884ca67b0dd0
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2026-02-04 23:48:35 +0100

    recycling bin bloc

--- lib/features/plastic/presentation/bloc/recycling_bloc.dart
+++ lib/features/plastic/presentation/bloc/recycling_bloc.dart
@@ -1,20 +1,39 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+//import 'package:recyscan/features/plastic/domain/entities/bin.dart';
+import 'package:recyscan/features/waste_scanning/domain/usecases/get_nearby_center.dart';
 import '../../domain/usecases/deposit_earn.dart';
+
 import 'recycling_event.dart';
 import 'recycling_state.dart';
 
 class RecyclingBloc extends Bloc<RecyclingEvent, RecyclingState> {
   final DepositPlasticAndEarn depositUseCase;
+  final GetNearbyCenters getNearbyBinsUseCase;
 
-  RecyclingBloc(this.depositUseCase) : super(RecyclingInitial()) {
+  RecyclingBloc({
+    required this.depositUseCase,
+    required this.getNearbyBinsUseCase,
+  }) : super(RecyclingInitial()) {
+    
+    // Handle loading bins for Map/AR
+    on<RefreshBinStatusEvent>((event, emit) async {
+      try {
+        //final bins = await getNearbyBinsUseCase();
+        //emit(BinsLoaded(bins.cast<SmartBin>()));
+      } catch (e) {
+        emit(RecyclingError("Failed to fetch smart bin status."));
+      }
+    });
+
+    // Handle the actual recycling transaction
     on<ConfirmDepositEvent>((event, emit) async {
       emit(RecyclingProcessing());
       try {
         final points = await depositUseCase.execute(event.binId, event.weight);
         emit(RecyclingRewardSuccess(points));
       } catch (e) {
-        emit(RecyclingError("Sensor timeout: Try again"));
+        
+        emit(RecyclingError(e.toString()));
       }
     });
   }
