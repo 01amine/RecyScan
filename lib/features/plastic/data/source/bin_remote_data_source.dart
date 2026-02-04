d7e1467ab00778f451c5a4be98f3b0f4e308ea1f
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2026-02-04 23:43:03 +0100

    test right remote

--- lib/features/plastic/data/source/bin_remote_data_source.dart
+++ lib/features/plastic/data/source/bin_remote_data_source.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:recyscan/core/constants/end_points.dart';
 
 import '../models/smart_bin_model.dart';
 
@@ -8,7 +9,7 @@ class BinRemoteDataSource {
   BinRemoteDataSource(this.client);
 
   Future<List<SmartBinModel>> fetchBins() async {
-    final response = await client.get(Uri.parse('https://api.recycle.com/bins'));
+    final response = await client.get(Uri.parse(EndPoints.recyclebin));
     if (response.statusCode == 200) {
       final List data = json.decode(response.body);
       return data.map((e) => SmartBinModel.fromJson(e)).toList();
@@ -19,7 +20,7 @@ class BinRemoteDataSource {
   Future<int> postDeposit(String binId, double weight) async {
     // Call backend to verify sensor triggered and update user points
     final response = await client.post(
-      Uri.parse('https://api.recycle.com/deposit'),
+      Uri.parse("${EndPoints.recyclebin}/deposit"),
       body: {'bin_id': binId, 'weight': weight.toString()},
     );
     return json.decode(response.body)['points_earned'];
